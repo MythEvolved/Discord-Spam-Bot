c930bfb97d7f53f554bca077d7d28125110c01b0
KazuyaShuu <74711612+MythEvolved@users.noreply.github.com>
2021-05-21 15:52:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Basic Config (for all bots)
-UserToken = [''] # User Token goes here. Multiple accounts can be used too. Example: 'token','token','token'.
-DiscordChannel = '' # Channel ID goes here.
+user_token = [''] # User Token goes here. Multiple accounts can be used too. Example: 'token','token','token'.
+discord_channel = '' # Channel ID goes here.
 
 # Picture Spam
 PicturesDirectory = '' # Pictures Directory goes here. Must contain double slash '\\' to work.
