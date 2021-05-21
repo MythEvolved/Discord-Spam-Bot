165414f7004d72a6c122bdce6864a2a65d03d114
KazuyaShuu <74711612+MythEvolved@users.noreply.github.com>
2021-05-21 16:01:42 +0300

    Update discord_spam_text.py

--- discord_spam_text.py
+++ discord_spam_text.py
@@ -13,7 +13,7 @@ async def on_ready():
   printf("Started Text Spam")
   while not client.is_closed:
     print(spam_text)
-    await client.send_message(discord.Object(id=DiscordChannel), spam_text)
+    await client.send_message(discord.Object(id=discord_channel), spam_text)
     await asyncio.sleep(time_between_messages) # Pause time between messages in seconds.
     
 client.run(token, bot=False)
